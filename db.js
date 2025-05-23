ab066a281f92b92be70ebf1f5e5f28d0fe7e0cef
Roy Yu <royyukingshing@gmail.com>
2025-05-23 21:41:56 +0800

    4th port change
    port change to 3006

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('jamming', 'admin', '12345678', {
     host: 'database-1.ctuas04ew5ro.ap-southeast-1.rds.amazonaws.com',
-    port: 3307,
+    port: 3306,
     dialect: 'mysql',
   });
 
