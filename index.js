e6f48b8ea56d034e907ca306c0d1664fb88f97e8
Roy Yu <royyukingshing@gmail.com>
2025-05-23 21:10:09 +0800

    2nd commit
    change index.js

--- index.js
+++ index.js
@@ -59,37 +59,38 @@ app.get('/api/db_sync', (req, res) => {
 // ------------ Create SSH tunnel and start server --------------
 
 
-const { createTunnel } = require('tunnel-ssh');
+// const { createTunnel } = require('tunnel-ssh');
 
-const tunnelOptions = { autoClose: true };
-const serverOptions = { port: 3307 }; // local port to forward
-const sshOptions = {
-  host: '18.136.210.109',
-  port: 22,
-  username: 'ubuntu',
-  privateKey: require('fs').readFileSync('/Users/yukingshing/Downloads/testing_key.pem')
-};
-const forwardOptions = {
-  srcAddr: '127.0.0.1',
-  srcPort: 3307,
-  dstAddr: 'database-1.ctuas04ew5ro.ap-southeast-1.rds.amazonaws.com',
-  dstPort: 3306
-};
+// const tunnelOptions = { autoClose: true };
+// const serverOptions = { port: 3307 }; // local port to forward
+// const sshOptions = {
+//   host: '18.136.210.109',
+//   port: 22,
+//   username: 'ubuntu',
+//   privateKey: require('fs').readFileSync('/Users/yukingshing/Downloads/testing_key.pem')
+// };
+// const forwardOptions = {
+//   srcAddr: '127.0.0.1',
+//   srcPort: 3307,
+//   dstAddr: 'database-1.ctuas04ew5ro.ap-southeast-1.rds.amazonaws.com',
+//   dstPort: 3306
+// };
 
-createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions)
-  .then(([server, conn]) => {
-    console.log('SSH tunnel established');
+// createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions)
+//   .then(([server, conn]) => {
+//     console.log('SSH tunnel established');
+
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
     })
     
-    // Now connect Sequelize to localhost:3307
+//     // Now connect Sequelize to localhost:3307
     
-    //   sequelize.authenticate()
-    //     .then(() => console.log('Database connected through SSH tunnel'))
-    //     .catch(err => console.error('Database connection error:', err));
+//     //   sequelize.authenticate()
+//     //     .then(() => console.log('Database connected through SSH tunnel'))
+//     //     .catch(err => console.error('Database connection error:', err));
 
-  })
-  .catch(err => {
-    console.error('Tunnel error:', err);
-  });
+//   })
+//   .catch(err => {
+//     console.error('Tunnel error:', err);
+//   });
